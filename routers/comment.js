a1c996c35ad897abf1bc6d04e7b05cf7726f4980
leubenia <hdodo777@gmail.com>
2021-10-21 22:32:41 +0900

    댓글 get요청

--- routers/comment.js
+++ routers/comment.js
@@ -4,6 +4,27 @@ const router = express.Router();
 const { sequelize } = require('../models');
 const midware = require('../middlewares/middles');
 
+//댓글 겟
+router.get('/:postId', async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const post =
+      'select comments.comment , comments.commentId, users.userName, users.userId , users.profile from comments inner join users on users.userId = comments.userId where postId = :postId;';
+    const comments = await sequelize.query(post, {
+      replacements: {
+        postId: postId
+      },
+      type: sequelize.QueryTypes.SELECT,
+    });
+    res.status(200).send({ result: 'success', comment : comments});
+  } catch (error) {
+    res.status(400).send({
+      errorMessage: '댓글 작성 실패',
+      result: 'fail',
+    });
+  }
+});
+
 //댓글 작성
 router.post('/:postId', midware, async (req, res) => {
   try {
